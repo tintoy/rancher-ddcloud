1f98a617db06493122070813bb554d5c7c565063
tintoy <tintoy@tintoy.io>
2016-11-15 10:44:25 +1100

    Load and save configuration.

--- setup.py
+++ setup.py
@@ -1,42 +1,74 @@
 #!/usr/bin/env python2
 
 import json
+import urllib2
+
+local_vars_path = './terraform/local-vars.json'
 
 def load_config():
-	variables = {}
+  variables = {}
 
-	try:
-		with open('./terraform/local-vars.json') as local_vars_file:
-			# Read local variables
-			local_vars_config = json.load(local_vars_file)['variable']
+  try:
+    with open(local_vars_path, 'r') as local_vars_file:
+      # Read local variables
+      local_vars_config = json.load(local_vars_file)['variable']
 
-			for variable_name in local_vars_config.keys():
-				variable_value = local_vars_config[variable_name]['default']
+      for variable_name in local_vars_config.keys():
+        variable_value = local_vars_config[variable_name]['default']
 
-				variables[variable_name] = variable_value
-	except IOError:
-		pass
+        variables[variable_name] = variable_value
+  except IOError:
+    pass # No existing configuration.
 
-	return variables
+  return variables
+
+def save_config(variables):
+  with open(local_vars_path, 'w') as local_vars_file:
+    local_vars_data = {
+      'variable': {}
+    }
+    for name, value in variables.items():
+      local_vars_data['variable'] = {
+        'name': {
+          'default': value
+        }
+      }
+
+    json.dump(local_vars_data, local_vars_file)
 
 def show_config(variables):
-	if len(variables) == 0:
-		return
+  if 'client_ip' in variables:
+    print('Client IP              = "{}"'.format(
+      variables['client_ip'])
+    )
+  if 'ssh_public_key_file' in variables:
+    print('SSH public key file    = "{}"'.format(
+      variables['ssh_public_key_file'])
+    )
+  if 'ssh_bootstrap_password' in variables:
+    print('SSH bootstrap password = "{}"'.format(
+      variables['ssh_bootstrap_password'])
+    )
 
-	print('Local configuration:')
-	if 'client_ip' in variables:
-		print('Client IP              = "{}"'.format(
-			variables['client_ip'])
-		)
-	if 'ssh_public_key_file' in variables:
-		print('SSH public key file    = "{}"'.format(
-			variables['ssh_public_key_file'])
-		)
-	if 'ssh_bootstrap_password' in variables:
-		print('SSH bootstrap password = "{}"'.format(
-			variables['ssh_bootstrap_password'])
-		)
-
-print('Examining configuration...')
 local_vars = load_config()
+
+if len(local_vars) > 0:
+  print('Existing configuration:\n')
+  show_config(local_vars)
+
+print('')
+print('=' * 80)
+print('')
+
+print('Detecting client IP...')
+response = json.loads(
+  urllib2.urlopen('http://ifconfig.co/json').read()
+)
+local_vars['client_ip'] = response['ip']
+
+print('')
+print('=' * 80)
+print('')
+
+print('Current configuration:\n')
 show_config(local_vars)
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,24 @@ provider "ddcloud" {
 	region = "AU"
 }
 
-variable "client_ip" 				{ } # Supply value in local-vars.json
-variable "ssh_public_key_file"		{ } # Supply value in local-vars.json
-variable "ssh_bootstrap_password"	{ } # Supply value in local-vars.json
+#################
+# Local variables
+#
+# Run setup.py to generate local-vars.json (where these values are supplied).
 
+variable "client_ip" 				{ }
+variable "ssh_public_key_file"		{ }
+variable "ssh_bootstrap_password"	{ }
+
+#########
 # Network
+
 variable "networkdomain_name"	{ default = "Rancher" }
 variable "primary_network"		{ default = "10.0.12.0/24" }
+
+#########
+# Outputs
+
+output "rancher_host_ip" {
+	value = "${ddcloud_nat.rancher_host.public_ipv4}"
+}
